731478e5de49b05fec9333e6b208a51a44ca9471
Rishi <124495375+rishicds@users.noreply.github.com>
2024-01-14 17:46:03 +0530

    Added Userlocation and IncidentForm components

---
+++ src/Components/IncidentList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Location from './Location';
+
+function IncidentList() {
+  // Implement logic to fetch and display incident list
+
+  return (
+    <div>
+      <h2>Incident List</h2>
+      {/* Display incident list here */}
+    </div>
+  );
+}
+
+export default IncidentList;
