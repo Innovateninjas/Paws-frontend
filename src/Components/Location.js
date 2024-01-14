1eebdf87705cbfd334c187a0b6d18cc9dc49cfa0
Rishi <124495375+rishicds@users.noreply.github.com>
2024-01-14 18:22:13 +0530

    add baseline shit

--- src/Components/Location.js
+++ src/Components/Location.js
@@ -1,6 +1,7 @@
 // Location.js
 
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Location() {
   const [userLocation, setUserLocation] = useState(null);
@@ -20,15 +21,18 @@ function Location() {
 
   return (
     <div>
-      <h2>User's Location</h2>
-      {userLocation ? (
-        <p>
-          Latitude: {userLocation.latitude}, Longitude: {userLocation.longitude}
-        </p>
-      ) : (
-        <p>Loading user location...</p>
-      )}
-    </div>
+  <h2>User's Location</h2>
+  {userLocation ? (
+    <p>
+      Latitude: {userLocation.latitude}, Longitude: {userLocation.longitude}
+    </p>
+  ) : (
+    <p>Loading user location...</p>
+  )}
+  <Link to="/report-incident">
+    <button>Report an incident</button>
+  </Link>
+</div>
   );
 }
 
