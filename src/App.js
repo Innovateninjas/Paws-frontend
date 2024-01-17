8014f9c54752a81ddb305103e71caaf92b464d49
Anirban Majumder <81350357+anirbanmajumder0@users.noreply.github.com>
2024-01-18 03:22:25 +0530

    added login and register pages

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Route,Routes} from 'react-router-dom';
 import IncidentForm from './Components/IncidentForm';
 import IncidentList from './Components/IncidentList';
 import Location from './Components/Location';
+import LoginForm from './Components/LoginForm';
+import RegisterForm from './Components/RegisterForm';
 
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<Location />} />
-      <Route path="/report-incident" element={<IncidentForm />} />
-      <Route path="/incident-list" element={<IncidentList />} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path="/" element={<Location />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/report-incident" element={<IncidentForm />} />
+        <Route path="/incident-list" element={<IncidentList />} />
+      </Routes>
+    </Router>
   );
 }
 
