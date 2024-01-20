8e063a91f177c825107bafd8ef6490ac0b717e23
Anirban Majumder <81350357+anirbanmajumder0@users.noreply.github.com>
2024-01-20 18:35:48 +0530

    fix img upload and added dark modeeeeee

--- src/Components/IncidentList.jsx
+++ src/Components/IncidentList.jsx
@@ -28,16 +28,16 @@ function IncidentList() {
       ) : (
         <ul>
           {reportedIncidents.map((incident) => (
-            <li key={incident.id}>
-              <strong>Name:</strong> {incident.user_name},{' '}
-              <strong>Phone Number:</strong> {incident.user_phone},{' '}
-              <strong>Email:</strong> {incident.user_email},{' '}
-              <strong>Animal Type:</strong> {incident.animal_type},{' '}
-              <strong>description:</strong> {incident.description},{' '}
-              <strong>image:</strong> {incident.image},{' '}
-              <strong>Condition:</strong> {incident.condition},{' '}
-              <strong>Landmark:</strong> {incident.landmark},{' '}
-              <strong>User's Location:</strong> Lat: {incident.latitude}, Lon: {incident.longitude}
+            <li key={incident.id} className="incident-box">
+              <img src={incident.image} alt={incident.description} />
+              <p><strong>Name:</strong> {incident.user_name}</p>
+              <p><strong>Phone Number:</strong> {incident.user_phone}</p>
+              <p><strong>Email:</strong> {incident.user_email}</p>
+              <p><strong>Animal Type:</strong> {incident.animal_type}</p>
+              <p><strong>Description:</strong> {incident.description}</p>
+              <p><strong>Condition:</strong> {incident.condition}</p>
+              <p><strong>Landmark:</strong> {incident.landmark}</p>
+              <p><strong>User's Location:</strong> Lat: {incident.latitude}, Lon: {incident.longitude}</p>
             </li>
           ))}
         </ul>
