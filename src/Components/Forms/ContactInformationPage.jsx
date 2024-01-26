416c36c7bfd6c49b2aac70afe2a9ceb64299bf3a
Rishi <124495375+rishicds@users.noreply.github.com>
2024-01-26 14:20:44 +0530

    Added auth to contact ContactInformationPage check @anirbanmajumder0

--- src/Components/Forms/ContactInformationPage.jsx
+++ src/Components/Forms/ContactInformationPage.jsx
@@ -1,8 +1,53 @@
-// ContactInformationPage.js
-
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function ContactInformationPage({ formData, errors, handleChange, handleNextPage, handleSubmit, validatePage }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      // Check if the user is logged in by verifying the presence of a CSRF token in localStorage
+      const token = localStorage.getItem('csrftoken');
+      console.log("CSRF Token:", token);
+      if (!token) {
+        // If not logged in, redirect to the login page
+        navigate("/register");
+      } else {
+        // If logged in, fetch user information and set it in the component's state
+        await fetchUserData();
+      }
+    };
+
+    checkLoginStatus();
+  }, [navigate]);
+
+  const fetchUserData = async () => {
+    try {
+      // Make an API call to fetch user information based on the CSRF token
+      const response = await fetch("https://aniresfr-backend.vercel.app/user", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('csrftoken')}`,
+        },
+      });
+
+      if (response.ok) {
+        const userData = await response.json();
+
+        // Set user information in the component's state
+        handleChange({ target: { name: "user_name", value: userData.name } });
+        handleChange({ target: { name: "user_phone", value: userData.phone } });
+        handleChange({ target: { name: "user_email", value: userData.email } });
+      } else {
+        // Handle error when fetching user information
+        console.error("Error fetching user information");
+      }
+    } catch (error) {
+      // Handle other errors
+      console.error("An error occurred:", error);
+    }
+  };
+
   return (
     <div>
       <h2>Page 3: Contact Information</h2>
