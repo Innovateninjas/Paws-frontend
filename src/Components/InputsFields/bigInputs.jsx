e7a241f0be0d1738ef07c4716d9130a9905a0d18
blacknut1803 <dp950810@gmail.com>
2024-01-29 19:20:11 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/Components/Forms/contactInformationPage/ContactInformationPage.jsx 	modified:   src/Components/Forms/contactInformationPage/ContactInformationPage.module.css 	modified:   src/Components/Forms/successPage/SuccessPage.jsx 	modified:   src/Components/Forms/successPage/SuccessPage.module.css 	modified:   src/Components/InputsFields/bigInputs.jsx 	modified:   src/Components/incidentPage/IncidentForm.jsx 	modified:   src/Components/landingpage/HomePage.jsx 	modified:   src/Components/landingpage/Homepage.module.css

--- src/Components/InputsFields/bigInputs.jsx
+++ src/Components/InputsFields/bigInputs.jsx
@@ -3,7 +3,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./biginputs.module.css";
 
-function InputField({ placeholder, type, value, onChange, required }) {
+function InputField({ name = "none", placeholder, type, value, onChange, required, backgroundColor, outline }) {
+    const inputStyle = {
+        backgroundColor: backgroundColor || 'white', // Set default background color to white if not provided
+        outline: outline ? '1px solid black' : 'none', // Apply outline style if outline prop is true
+    };
+
     return (
         <input
             className={styles.input}
@@ -12,16 +17,25 @@ function InputField({ placeholder, type, value, onChange, required }) {
             value={value}
             onChange={onChange}
             required={required}
+            style={inputStyle}
+            name={name} // Add the name attribute
         />
     );
 }
 
 InputField.propTypes = {
+    name: PropTypes.string, // Add name prop type
     placeholder: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired,
     required: PropTypes.bool,
+    backgroundColor: PropTypes.string,
+    outline: PropTypes.bool, // Add outline prop type
+};
+
+InputField.defaultProps = {
+    outline: false, // Set default value of outline prop to false
 };
 
 export default InputField;
