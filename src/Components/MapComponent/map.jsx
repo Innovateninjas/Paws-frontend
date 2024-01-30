8273ad58c099216773e059c80d8e5f27f3e7cfe2
blacknut1803 <dp950810@gmail.com>
2024-01-30 18:10:29 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/Components/Forms/imageAndLocationPage/ImageAndLocationPage.jsx 	modified:   src/Components/Forms/imageAndLocationPage/ImageAndLocationPage.module.css 	new file:   src/Components/MapComponent/map.jsx 	new file:   src/Components/MapComponent/map.module.css

---
+++ src/Components/MapComponent/map.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+function MyComponent({ containerStyle, center, zoom }) {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyBBUSExqFtg19K7UZQ4LzGE7MygnoxibRo"
+    })
+
+    const [map, setMap] = React.useState(null)
+
+    const onLoad = React.useCallback(function callback(map) {
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+
+        setMap(map)
+    }, [center])
+
+    const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+    }, [])
+
+    const mapOptions = {
+        mapTypeId: 'satellite' // Set default map type to satellite
+    };
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={zoom}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+            options={mapOptions} // Pass the map options here
+        >
+            { /* Child components, such as markers, info windows, etc. */}
+            <></>
+        </GoogleMap>
+    ) : <></>
+}
+
+export default React.memo(MyComponent);
