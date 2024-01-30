412a23dd7b0685222c565974a8acb419e940cd5c
Rishi <124495375+rishicds@users.noreply.github.com>
2024-01-30 12:35:48 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
 );
 // src/index.js
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
+  navigator.serviceWorker.register('../public/service-worker.js')
     .then((registration) => {
       console.log('Service Worker registered with scope:', registration.scope);
     })
