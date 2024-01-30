ea73e53a95d646fd134d2b7e99ff82032572812f
Rishi <124495375+rishicds@users.noreply.github.com>
2024-01-30 12:23:54 +0530

    add pwa capability to react app

--- src/index.js
+++ src/index.js
@@ -10,6 +10,17 @@ root.render(
     <App />
   </React.StrictMode>
 );
+// src/index.js
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
