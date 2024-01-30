e3c0eeb731d11d01da051ccaabff6605e5570ef3
Rishi <124495375+rishicds@users.noreply.github.com>
2024-01-30 21:32:36 +0530

    Refactor image upload function and handle errors

--- src/Components/incidentPage/IncidentForm.jsx
+++ src/Components/incidentPage/IncidentForm.jsx
@@ -183,31 +183,6 @@ function IncidentForm() {
 
  
   // Function to upload image to Cloudinary
-  const uploadImageToCloudinary = async (imageFile) => {
-    const cloudinaryUploadUrl = "https://api.cloudinary.com/v1_1/dff97ky68/upload";
-    const uploadPreset = "mnxkqfco";
-
-    const formData = new FormData();
-    formData.append("file", imageFile);
-    formData.append("upload_preset", uploadPreset);
-
-    try {
-      const response = await fetch(cloudinaryUploadUrl, {
-        method: "POST",
-        body: formData,
-      });
-
-      if (!response.ok) {
-        throw new Error(`Failed to upload image to Cloudinary: ${response.statusText}`);
-      }
-
-      const result = await response.json();
-      return result.secure_url; // Assuming Cloudinary API returns an object with a 'secure_url' property
-    } catch (error) {
-      console.error("Error uploading image to Cloudinary:", error);
-      throw error; // Propagate the error
-    }
-  };
 
   const renderPage = () => {
     switch (currentPage) {
