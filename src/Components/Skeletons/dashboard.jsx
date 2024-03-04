0e1b591d9730652b3cbc5584f26066aad54890c8
Arnab Mondal <dp950810@gmail.com>
2024-03-04 23:52:02 +0530

    modified:   src/Components/Skeletons/dashboard.jsx 	modified:   src/pages/dashboardPage/dashboard.jsx

--- src/Components/Skeletons/dashboard.jsx
+++ src/Components/Skeletons/dashboard.jsx
@@ -5,16 +5,16 @@ const DashboardSkeleton = props => (
     <ContentLoader
         speed={3}
         opacity={0.1}
-        height={280}
+        height={300}
         width="100%"
         backgroundColor="#b8b5b5;" // Set background color
         foregroundColor="#ffffff" // Set foreground color to white for better contrast
         {...props}
     >
-        <rect x="43" y="3" rx="15" ry="15" width="305" height="180" /> {/* Adjust x position */}
-        <rect x="43" y="190" rx="10" ry="10" width="200" height="20" /> {/* Adjust x position */}
-        <rect x="43" y="215" rx="10" ry="10" width="249" height="20" /> {/* Adjust x position */}
-        <rect x="43" y="242" rx="10" ry="10" width="284" height="20" /> {/* Adjust x position */}
+        <rect x="43" y="3" rx="15" ry="15" width="310" height="200" /> {/* Adjust x position */}
+        <rect x="43" y="210" rx="10" ry="10" width="200" height="20" /> {/* Adjust x position */}
+        <rect x="43" y="235" rx="10" ry="10" width="249" height="20" /> {/* Adjust x position */}
+        <rect x="43" y="262" rx="10" ry="10" width="284" height="20" /> {/* Adjust x position */}
     </ContentLoader>
 )
 
