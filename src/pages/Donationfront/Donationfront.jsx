00f4eeee10ea6f662f6cc8023166bac781464a12
rishicds <124495375+rishicds@users.noreply.github.com>
2024-03-08 01:13:56 +0530

    fixed ngo search thingy rest will be done by @debayudh07 half done UserPage rest will be done by @GhoshAditi

--- src/pages/Donationfront/Donationfront.jsx
+++ src/pages/Donationfront/Donationfront.jsx
@@ -1,47 +1,46 @@
 import React, { useState , useEffect } from 'react';
 import styles from './Donationfront.module.css';
 import { Link } from 'react-router-dom';
-//import { NgoContext } from "../../contexts/NgoContext";
 
 export const Donationfront = () => {
   const [theme, setTheme] = useState({
     startColor: '#ff105f',
     endColor: '#ffad06',
   });
-    const [donationType, setDonationType] = useState('us');
-    const [showContent, setShowContent] = useState(true);
-    const [ngoNames, setNgoNames] = useState([]);
+  const [donationType, setDonationType] = useState('us');
+  const [showContent, setShowContent] = useState(true);
+  const [ngoNames, setNgoNames] = useState([]);
   
-    const handleToggleClick = (type) => {
-      setDonationType(type);
-      setShowContent(true);
-      setTheme(type === 'us' ? { startColor: '#ff105f', endColor: '#ffad06' } : { startColor: '#00bcd4', endColor: '#ff5722' });
+  const handleToggleClick = (type) => {
+    setDonationType(type);
+    setShowContent(true);
+    setTheme(type === 'us' ? { startColor: '#ff105f', endColor: '#ffad06' } : { startColor: '#00bcd4', endColor: '#ff5722' });
+  };
+
+  useEffect(() => {
+    const fetchNgoNames = async () => {
+      try {
+        const response = await fetch('https://aniresfr-backend.vercel.app/ngo');
+        if (response.ok) {
+          const data = await response.json();
+          console.log("ngo names :", data);
+          setNgoNames(data); // Assuming the response is an array of objects with a 'name' property
+        } else {
+          console.error('Failed to fetch NGO names');
+        }
+      } catch (error) {
+        console.error('Error fetching NGO names:', error);
+      }
     };
 
-    useEffect(() => {
-      // Fetch NGO names from the server
-      const fetchNgoNames = async () => {
-        try {
-          const response = await fetch('https://aniresfr-backend.vercel.app/ngo');
-          if (response.ok) {
-            const data = await response.json();
-            console.log("ngo names :", data);
-            setNgoNames(data); // Assuming the response is an array of strings
-          } else {
-            console.error('Failed to fetch NGO names');
-          }
-        } catch (error) {
-          console.error('Error fetching NGO names:', error);
-        }
-      };
-  
-      fetchNgoNames();
-    },[]);
-    return (
-      <div>
-        <div className={styles.hero}>
-          <div className={styles.formBox}>
-            <div className={styles.boxbutton}>
+    fetchNgoNames();
+  }, []);
+
+  return (
+    <div>
+      <div className={styles.hero}>
+        <div className={styles.formBox}>
+          <div className={styles.boxbutton}>
             <div className={styles.buttonBox}>
               <div
                 id='btn'
@@ -60,41 +59,43 @@ export const Donationfront = () => {
               <button
                 type='button'
                 className={`${styles.togglebtn} ${donationType === 'ngo' ? styles.active : ''}`}
-              onClick={() => handleToggleClick('ngo')}
+                onClick={() => handleToggleClick('ngo')}
               >
                 Donate to NGO
               </button>
             </div>
-            </div>
-            {showContent && (
-              <div className={donationType === 'us' ? styles.usText : styles.ngoText}>
-                <p className={styles.paragraph}>
-                  {donationType === 'us' ? 'Donate to us' : 'Donate to your CHOICE OF NGO'}
-                  <br />
-                  {donationType === 'us' && 'To help us save our little friends!'}
-                </p>
-                {donationType === 'ngo' && (
-                  <select>
-                     <option>Choose an NGO</option>
-                  {ngoNames.map((ngoName, index) => (
-                    <option key={index} value={ngoName}>
-                      {ngoName}
+          </div>
+          {showContent && (
+            <div className={donationType === 'us' ? styles.usText : styles.ngoText}>
+              <p className={styles.paragraph}>
+                {donationType === 'us' ? 'Donate to us' : 'Donate to your CHOICE OF NGO'}
+                <br />
+                {donationType === 'us' && 'To help us save our little friends!'}
+              </p>
+              {donationType === 'ngo' && (
+                <select>
+                  <option>Choose an NGO</option>
+                  {ngoNames.map((ngo, index) => (
+                    <option key={index} value={ngo.name}>
+                      {ngo.name}
                     </option>
                   ))}
-                  </select>
-                 )}
-                  {/* fetch the ngo name list from the backend in the above function  */}
-                <div className={styles.donateBtn}>
-                  <button
-                    type='submit'
-                    className= {styles.submitbtn}
-                  > <Link className={styles.lin} to="/donation">Donate</Link>
-                  </button>
-                </div>
+                </select>
+              )}
+              <div className={styles.donateBtn}>
+                <button
+                  type='submit'
+                  className={styles.submitbtn}
+                >
+                  <Link className={styles.lin} to="/donation">Donate</Link>
+                </button>
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
+
+export default Donationfront;
--- src/pages/UserProfilePage/UserPage.jsx
+++ src/pages/UserProfilePage/UserPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { FiUser, FiMail, FiPhone } from 'react-icons/fi';
+import { FiUser, FiMail, FiPhone, FiDollarSign, FiAward } from 'react-icons/fi';
 import ProfileIcon from '../../Components/ProfileComponent/ProfileIcon';
 import styles from './UserPage.module.css';
 import Loader from '../../Components/loader/loader';
@@ -8,42 +8,49 @@ import { UserContext } from '../../contexts/UserContext';
 
 function UserPage() {
   const [userDetails, setUserData] = useState(null);
-  const { userData, loading, error } = useContext(UserContext)
+  const { userData, loading, error } = useContext(UserContext);
+  
   useEffect(() => {
     if (!loading && !error && userData) {
-      setUserData(userData)
+      setUserData(userData);
     }
   }, [userData, loading, error]);
 
   if (error) {
-    return <h1>{error}</h1>
+    return <h1>{error}</h1>;
   }
 
   if (loading) {
     return <Loader visible />;
   }
+
   if (userDetails) {
     return (
       <div className={styles.container}>
         <div className={styles.profileContainer}>
           <div className="wrap">
-            <ProfileIcon top={"20%"} borderVisible position={'absolute'} />
+            {/* Profile picture */}
+            <div className={styles.profilepic}><ProfileIcon /></div>        
+            {/* Display name and email below the profile picture */}
+            <p className={styles.username}><FiUser /> <span className={styles.name}>{userDetails.name}</span></p>
+            <p className={styles.email}><FiMail /> <span className={styles.email}>{userDetails.email}</span></p>
+            <p className={styles.phone}><FiPhone /> <span className={styles.phone}>{userDetails.phone_number}</span></p>
           </div>
         </div>
         <div className={styles.userDetails}>
-          {/* Icon for Name */}
-          <p><FiUser /> <span className={styles.name}>{userDetails.name}</span></p>
-          {/* Icon for Email */}
-          <p><FiMail /> <span className={styles.email}>{userDetails.email}</span></p>
           {/* Icon for Phone */}
-          <p><FiPhone /> <span className={styles.phone}>{userDetails.phone_number}</span></p>
+          
           {/* There are more details about the user present in userDetail object console log to seen them and show them in the ui  */}
-          <p>
-
-            <Link to="/view-reports">View Reports</Link> {/* Add this line */}
-          </p>
+          <p className={styles.levels}><FiAward /> <span className={styles.level}>Level: {userDetails.level}</span></p>
+          {/* User Coins */}
+          <p className={styles.coins}><FiDollarSign /> <span className={styles.coins}>Coins: {userDetails.coins}</span></p>
+          <button className={styles.button}>
+          <Link to="/view-reports">My Reports</Link>
+        </button>
+        <button className={styles.button}>
+          <Link to="/logout">Logout</Link>
+        </button>
           <br />
-          <p>level,coins, no of reports ,logout and more to add here </p>
         </div>
       </div>
     );
