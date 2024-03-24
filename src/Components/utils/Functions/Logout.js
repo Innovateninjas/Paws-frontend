44fdaea9f5ca52bc79b945c1eea629bc67110237
Anirban Majumder <81350357+anirbanmajumder0@users.noreply.github.com>
2024-03-24 18:22:46 +0530

    Added proper logout logic
    i.e. remove the token from backend

--- src/Components/utils/Functions/Logout.js
+++ src/Components/utils/Functions/Logout.js
@@ -1,15 +1,29 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Logout() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    localStorage.clear(); // clears both userData and csrf token at once 
-    navigate('/');
-    window.location.reload();
+    const csrftoken = localStorage.getItem("csrftoken");
+    axios.post('https://aniresfr-backend.vercel.app/logout/', {}, {
+      headers: {
+          'Authorization': `Token ${csrftoken}`,
+      },
+      withCredentials: true
+    })
+    .then(() => {
+      localStorage.clear(); // clears both userData and csrf token at once 
+      navigate('/');
+      window.location.reload();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   }, [navigate]);
 
+  return null;
 }
 
 export default Logout;
