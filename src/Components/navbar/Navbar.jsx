7c172071c5d18bf43e7ca4afbf44e5e95b5a73e1
Anirban Majumder <81350357+anirbanmajumder0@users.noreply.github.com>
2024-03-25 16:13:13 +0530

    Navbar 2.O
    removed all redundant logic also added icons (as close to figma as possible) and fix the active/select route issue.

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -2,83 +2,81 @@ import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import { FaUser, FaUserPlus, FaHome, FaDonate } from 'react-icons/fa';
-import styles from './Navbar.module.css';
-import { MdPeople, MdCampaign } from 'react-icons/md';
+import { FaUser, FaUserPlus} from 'react-icons/fa';
+import { PiHandCoinsBold } from "react-icons/pi";
+import { MdCampaign, MdHome, MdContentPasteSearch } from "react-icons/md";
+import { IoPeople } from "react-icons/io5";
 import { ImStatsBars } from "react-icons/im";
-import { TbReportSearch } from "react-icons/tb";
+
+//function for replacing the icons with images or svg
+//const NavigationAction = ({ value, to, imgSrc, key }) => {
+//  const imgPath = `images/nav/${imgSrc}.png`;
+//
+//  return (
+//    <BottomNavigationAction 
+//      value={value} 
+//      component={Link} 
+//      to={to} 
+//      icon={<img src={imgPath} alt={key} style={{width: '32px', height: '32px'}} />} 
+//    />
+//  );
+//};
+
 
 const Navbar = () => {
   const csrftoken = localStorage.getItem('csrftoken');
   const userType = localStorage.getItem('userType');
   const [value, setValue] = useState(0);
-  const [showNavbar, setShowNavbar] = useState(true);
   const location = useLocation();
+  const [isScrollable, setIsScrollable] = useState(false);
 
   useEffect(() => {
-    const handleScroll = () => {
-      const isBottom = window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight;
-      const isAlwaysVisiblePage = [
-        '/user',
-        '/donationfront',
-        '/',
-        '/report-incident',
-        '/stats',
-        '/createcampaign',
-        '/ngoProfile',
-        '/dashboard',
-        '/login',
-        '/register'
-      ].includes(location.pathname);
-
-      setShowNavbar(isAlwaysVisiblePage || !isBottom);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
+    setValue(location.pathname);
+    setIsScrollable(window.innerHeight < document.body.offsetHeight);
   }, [location.pathname]);
 
-  // Update the value state when location changes
-  useEffect(() => {
-    const paths = ['/', '/user', '/donationfront', '/campaignList'];
-    setValue(paths.indexOf(location.pathname));
-  }, [location.pathname]);
-
-  return showNavbar ? (
-    <nav className={styles.navbar}>
+  return (
+    <div className={`fixed bottom-0 left-0 w-full z-10 ${isScrollable ? 'bg-white' : 'bg-opacity-0'}`}>
+    <nav className='p-0.5 bg-teal-500/25'>
       <BottomNavigation
         value={value}
         onChange={(event, newValue) => {
           setValue(newValue);
         }}
+        style={{
+          width: '100%',
+          margin: '0',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-evenly',
+          backgroundColor: 'transparent',
+        }}
       >
         {csrftoken && userType === "user" ? (
           [
-            <BottomNavigationAction key="home" component={Link} to="/" icon={<FaHome size={24} />} />,
-            <BottomNavigationAction key="profile" component={Link} to="/user" icon={<FaUser size={24} />} />,
-            <BottomNavigationAction key="donationfront" component={Link} to="/donationfront" icon={<FaDonate size={24} />} />,
-            <BottomNavigationAction key="campaignList" component={Link} to="/campaignList" icon={<MdPeople size={24} />} />,
+            <BottomNavigationAction key="home" value="/" component={Link} to="/" icon={<MdHome size={30}/>} />,
+            <BottomNavigationAction key="campaignList" value="/campaignList" component={Link} to="/campaignList" icon={<IoPeople size={28} />} />,
+            <BottomNavigationAction key="donationfront" value="/donationfront" component={Link} to="/donationfront" icon={<PiHandCoinsBold size={28} />} />,
+            <BottomNavigationAction key="profile" value="/user" component={Link} to="/user" icon={<FaUser size={24} />} />,
           ]
         ) : csrftoken && userType === "ngo" ? (
           [
-            <BottomNavigationAction key="stats" component={Link} to="/stats" icon={<ImStatsBars size={24} />} />,
-            <BottomNavigationAction key="reports" component={Link} to="/dashboard" icon={<TbReportSearch size={24} />} />,
-            <BottomNavigationAction key="createcampaign" component={Link} to="/createcampaign" icon={<MdCampaign size={24} />} />,
-            <BottomNavigationAction key="ngoprofile" component={Link} to="/ngoProfile" icon={<FaUser size={24} />} />,
+            <BottomNavigationAction key="stats" value="/stats" component={Link} to="/stats" icon={<ImStatsBars size={26}/>}/>,
+            <BottomNavigationAction key="reports" value="/dashboard" component={Link} to="/dashboard" icon={<MdContentPasteSearch size={28}/>} />,
+            <BottomNavigationAction key="createcampaign" value="/createcampaign" component={Link} to="/createcampaign" icon={<MdCampaign size={32} style={{ transform: 'rotate(-15deg)' }} />} />,
+            <BottomNavigationAction key="ngoprofile" value="/ngoProfile" component={Link} to="/ngoProfile" icon={<FaUser size={24} />} />,
           ]
         )
           : (
             [
-              <BottomNavigationAction key="register" showLabel label="Register" component={Link} to="/register" icon={<FaUserPlus size={24} />} />,
-              <BottomNavigationAction key="login" showLabel label="Login" component={Link} to="/login" icon={<FaUser size={24} />} />,
+              <BottomNavigationAction key="register" value="/register" showLabel label="Register" component={Link} to="/register" icon={<FaUserPlus size={28} />} />,
+              <BottomNavigationAction key="login" value="/login" showLabel label="Login" component={Link} to="/login" icon={<FaUser size={24} />} />,
             ]
           )}
       </BottomNavigation>
     </nav>
-  ) : null;
+    </div>
+  );
 };
 
 export default Navbar;
