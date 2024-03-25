2af4d91b3229c1b152b0183b11add15a001ced2d
codewarnab <dp950810@gmail.com>
2024-03-25 15:14:34 +0530

    deleted:    public/fonts/Candle-light.ttf 	modified:   public/index.html 	modified:   src/Components/Skeletons/dashboard.jsx 	modified:   src/pages/dashboardPage/dashboard.jsx

--- src/Components/Skeletons/dashboard.jsx
+++ src/Components/Skeletons/dashboard.jsx
@@ -11,10 +11,10 @@ const DashboardSkeleton = props => (
         foregroundColor="#ffffff" // Set foreground color to white for better contrast
         {...props}
     >
-        <rect x="43" y="3" rx="15" ry="15" width="310" height="200" /> {/* Adjust x position */}
-        <rect x="43" y="210" rx="10" ry="10" width="200" height="20" /> {/* Adjust x position */}
-        <rect x="43" y="235" rx="10" ry="10" width="249" height="20" /> {/* Adjust x position */}
-        <rect x="43" y="262" rx="10" ry="10" width="284" height="20" /> {/* Adjust x position */}
+        <rect x="13" y="3" rx="15" ry="15" width="330" height="200" /> 
+        <rect x="16" y="210" rx="10" ry="10" width="200" height="20" /> 
+        <rect x="16" y="262" rx="10" ry="10" width="284" height="20" /> 
+        <rect x="16" y="235" rx="10" ry="10" width="249" height="20" /> 
     </ContentLoader>
 )
 
