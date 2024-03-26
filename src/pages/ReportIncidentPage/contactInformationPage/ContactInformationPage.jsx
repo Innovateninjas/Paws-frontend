cd2bf950cbfbbf29cb4dbd25ecfb6a332e87983e
Shreya <shreyashaw1705@gmail.com>
2024-03-26 23:32:19 +0530

    Contact Details

--- src/pages/ReportIncidentPage/contactInformationPage/ContactInformationPage.jsx
+++ src/pages/ReportIncidentPage/contactInformationPage/ContactInformationPage.jsx
@@ -10,6 +10,7 @@ function ContactInformationPage({
   formData,
   setFormData,
   errors,
+  handleBackPage,
   handleChange,
   handleSubmit,
   validatePage,
@@ -34,64 +35,59 @@ function ContactInformationPage({
   <div className="h-screen w-screen font-breeSerif">
     <Background />
     <div className=" flex flex-col justify-center items-center relative">
-      <div className="mt-24 relative w-full h-screen gap-[20px] flex flex-col items-center ">
+      <div className="mt-24 relative w-full  gap-[20px] flex flex-col items-center ">
         <h1 className="text-center mt-[20px] pb-1 z-[3] text-[#0B0553DE] font-black tracking-wide text-[1.6em] underline">CONTACT DETAILS</h1>
-
-     <div className="bg-slate-100 w-[90%] p-5 rounded-3xl bg-opacity-50 backdrop-blur-[6px] flex flex-col justify-evenly">
+     <div className="bg-white w-[90%] p-5 rounded-3xl bg-opacity-57 backdrop-blur-[6px] flex flex-col justify-evenly gap-[10px]">
    <div className="flex flex-col gap-[10px]">
         <label className="font-extrabold text-[#0B0553DE] pl-3 text-xl">
           Name :
           </label>
           <InputField
-           className=" h-[60px] rounded-[30px] p-4 w-[100%] outline-0 bg-opacity-80 bg-[#65D33C] shadow-custom"
+           className=" h-[60px] rounded-[30px] text-white p-4 w-[100%] outline-0 bg-opacity-80 bg-gradient-to-r from-green-500 to-[#65D33C] shadow-dashBoardCardImageShadow"
             type="text"
             name="user_name"
-            placeholder="Name"
             value={formData.user_name}
             onChange={handleChange}
             required={true}
             backgroundColor={"#D9D9D9"}
             outline={true}
           />
+          {console.log(errors)}
+          <small className="text-sm text-red-500">{errors.user_name}</small>
           <div className="error">{errors.first_name}</div>
           </div>
-        <br />
         <div className="flex flex-col gap-[10px]">
         <label className="font-extrabold text-[#0B0553DE] pl-3 text-xl">
           Phone Number:</label>
           <InputField
- className=" h-[60px] rounded-[30px] p-4 w-[100%] outline-0 bg-opacity-80 bg-[#A15ECF] shadow-custom"
+         className=" h-[60px] rounded-[30px] p-4 w-[100%] outline-0 bg-opacity-80  bg-slate-200 shadow-dashBoardCardImageShadow"
             type="tel"
             name="user_phone"
-            placeholder="Phone Number"
             value={formData.user_phone}
             onChange={handleChange}
             required={true}
-            // backgroundColor={"#D9D9D9"}
             outline={true}
           />
-          <div className="error">{errors.last_name}</div>
+          <small className="text-sm text-red-500">{errors.user_phone}</small>
           </div>
-        <br />
         <div className="flex flex-col gap-[10px]">
         <label className="font-extrabold text-[#0B0553DE] pl-3 text-xl">
           Email: </label>
           <InputField
- className=" h-[60px] rounded-[30px]  p-4 w-[100%] outline-0 bg-opacity-80 bg-[#7096F5] shadow-custom"
+ className=" h-[60px] rounded-[30px] text-white  p-4 w-[100%] outline-0 backdrop-blur-[6px] bg-gradient-to-r from-blue-500 to-[#7096F5] shadow-dashBoardCardImageShadow"
             type="email"
             name="user_email"
-            placeholder="Email"
             value={formData.user_email}
             onChange={handleChange}
             required={true}
           />
-          <div className="error">{errors.username}</div>
+          <small className="text-sm text-red-500">{errors.user_email}</small>
 </div>
         </div>
-        <p className="text-breeSerif font-medium text-center w-9/10 mx-auto text-blue-900 cursor-pointer underline" onClick={() => setModalIsOpen(true)}>Learn why we need that data</p>
+        <p className="text-breeSerif font-medium text-center w-9/10 mx-auto text-violet-900 cursor-pointer underline text-[18px]" onClick={() => setModalIsOpen(true)}>Learn why we need that data</p>
         <div className="flex justify-evenly  w-full">
-          <Button text="Back" clas="bg-blue-900 text-white" onClick={() => {console.log('Clicked on Back');}}/>
-          <Button text="Submit" clas=" bg-gradient-to-b from-indigo-300 to-indigo-800 text-baloo" onClick={(e) => {
+          <Button text="Back" clas="text-base text-white focus:outline-none rounded-[30px] shadow-buttonShadow font-medium tracking-widest bg-gradient-to-b from-green-300 to-green-800" onClick={handleBackPage}/>
+          <Button text="Submit" clas="text-base text-white bg-gradient-to-b from-blue-300 to-blue-800 shadow-buttonShadow focus:outline-none rounded-[30px]  bg-opacity-20 font-medium tracking-widest" onClick={(e) => {
             if (validatePage()) {
               handleNextPage();
               handleSubmit(e);
