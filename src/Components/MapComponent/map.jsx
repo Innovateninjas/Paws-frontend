42c3caa4a6b25e14601047bedb3c6350c17493bc
rishicds <124495375+rishicds@users.noreply.github.com>
2024-03-26 16:36:23 +0530

    map improved?

--- src/Components/MapComponent/map.jsx
+++ src/Components/MapComponent/map.jsx
@@ -1,31 +1,53 @@
 import React, { useState ,useEffect} from 'react';
 import PropTypes from 'prop-types';
-import { MapContainer, TileLayer } from 'react-leaflet'; // docs link https://react-leaflet.js.org/docs/start-introduction/
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'; // docs link https://react-leaflet.js.org/docs/start-introduction/
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 const Map = ({ zoom = 13, customCenter }) => {
     // Generate a unique key whenever customCenter changes
     const [key, setKey] = useState(0);
+    const [location, setLocation] = useState(null);
 
     // Update the key whenever customCenter changes
     useEffect(() => {
         setKey(prevKey => prevKey + 1);
     }, [customCenter]);
 
+    // Get current location
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            setLocation([position.coords.latitude, position.coords.longitude]);
+        });
+    }, []);
+
     return (
         <div className="map-container w-full h-full">
             <MapContainer className='w-[335px] h-full flex flex-col items-center justify-center rounded-lg'
                 key={key}
-                center={customCenter}
+                center={customCenter || location}
                 zoom={zoom}
                 attributionControl={false}
                 preferCanvas={false}
-                
             >
                 <TileLayer
-                    url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="&copy; Pawss"
                 />
+                {location && (
+                    <Marker position={location}>
+                        <Popup>
+                            The animal is here!
+                        </Popup>
+                    </Marker>
+                )}
             </MapContainer>
         </div>
     );
@@ -33,7 +55,7 @@ const Map = ({ zoom = 13, customCenter }) => {
 
 Map.propTypes = {
     zoom: PropTypes.number,
-    customCenter: PropTypes.arrayOf(PropTypes.number),
+    customCenter: PropTypes.array,
 };
 
 export default Map;
