7c275b69aa7953228b3d040c6a26b16f500f0256
Shreya <shreyashaw1705@gmail.com>
2024-03-26 21:43:14 +0530

    AnimalDetailsPage

--- src/Components/tailwindButton/Button.jsx
+++ src/Components/tailwindButton/Button.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import {twMerge } from 'tailwind-merge';
 const Button = ({ text, clas, onClick }) => {
+
+    // text-base mt-3 text-white  focus:outline-none rounded-[30px] px-5 py-4 bg-opacity-20 font-semibold
     var baseClass = "text-base focus:outline-none rounded-[30px] px-10 py-4 font-semibold ";  
     return (
         <button className={twMerge(baseClass, clas)} onClick={onClick}>{text}</button>
