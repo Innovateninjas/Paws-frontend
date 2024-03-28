830e0cd105b07fec6d222dabb01cf25c2242fcde
Shreya <shreyashaw1705@gmail.com>
2024-03-28 23:14:25 +0530

    Create Campaign Page

--- src/pages/CampaignNgoPage/CreateCampaign/createCampaignPage.jsx
+++ src/pages/CampaignNgoPage/CreateCampaign/createCampaignPage.jsx
@@ -3,21 +3,25 @@ import React from "react";
 import { FaPlus } from "react-icons/fa";
 import styles from "./createCampaign.module.css";
 import MButton from "../../../Components/buttons/MediumButton/mButton";
+import Background from "../../../Components/backgroundComponent/Background";
 
 const CreateCampaignPage = ({ onClick }) => {
-    return (
-        <div className={styles.container}>
-            <img src="./images/paw.png" alt="paw img" className={styles.paw3} />
-            <h1 className={styles.heading1}>
-                <p className={styles.slogan}>
-                    Protecting Paws,<br></br> Saving Lives
-                </p>
-            </h1>
-            {/* Remove one onClick prop */}
-            <MButton text="Create Campaign"  onClick={onClick} />
-            <img src="./images/paw.png" alt="paw img" className={styles.paw4} />
+  return (
+    <>
+      <Background />
+      <div className=" h-[60vh] flex z-[60] items-center justify-between flex-col">
+        <h1 className="mt-[40px] font-semibold text-center w-[90vw] backdrop-blur-[6px] font-BigShotOne text-shadow shadow-custom bg-[#1e84e481] rounded-[40px] py-[10px] px-[20px]">
+          <p className="text-3xl flex flex-col gap-5">
+            <span>Protecting Paws,</span>
+            <span>Saving Lives</span>
+          </p>
+        </h1>
+        <div>
+          <MButton text="Create Campaign" onClick={onClick} />
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default CreateCampaignPage;
