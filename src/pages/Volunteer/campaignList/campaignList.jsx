c7939fd46c99cf2af84422ac7e5bf4889ee47584
Shreya <shreyashaw1705@gmail.com>
2024-03-31 14:40:56 +0530

    Update campaignList.jsx

--- src/pages/Volunteer/campaignList/campaignList.jsx
+++ src/pages/Volunteer/campaignList/campaignList.jsx
@@ -29,20 +29,21 @@ const Campaignlist = () => {
     <Background/>
       {!isLoading && (
         <>
-        <div className="flex flex-col items-center justify-center gap-[20px] mb-[60px] w-screen ">
-          <h1 className="text-center mt-[30px] pb-1 z-[3] text-[#40025D] font-bold tracking-wide text-[2em] ">
-            Volunteer Now,<br></br> Make Your Mark!</h1>
+        <div className="flex flex-col items-center font-breeSerif justify-center gap-[20px] mb-[60px] w-screen ">
+          <h1 className="text-center mt-[30px] pb-1 z-[3] text-[#40025D] font-extrabold tracking-widest text-4xl drop-shadow-xl flex flex-col">
+           <span className="mb-[5px]">Volunteer Now,</span><span> Make Your Mark!</span></h1>
           <div className={styles.masterContainer}>
           {data.map((item, index) => (
             <div key={index} className="flex justify-center w-screen">
-            <div  className=" py-[15px] px-[20px] w-[90%] flex flex-col gap-[10px] rounded-3xl shadow-dashBoardCardImageShadow bg-[#ffffff66]  backdrop-blur-[5px] mb-[20px]">
-              <div className="flex flex-col gap-4">
-                <p className="font-bold p-2 tracking-wide text-[1.3rem] text-[#0b0553de]">{item.title}</p>
+            <div  className=" py-[15px] px-[20px] w-[90%] flex flex-col rounded-3xl shadow-dashBoardCardImageShadow bg-[#ffffff66]  backdrop-blur-[5px] mb-[20px]">
+              <div className="flex flex-col gap-3">
+              {/* TITLE */}
+                <p className="font-bold drop-shadow-md tracking-wide text-[1.3rem] text-[#0b0553de]">{item.title}</p>
                 {/* DURATION */}
-                <div className="flex flex-col gap-2">
-                <small className="text-[#092817] font-medium text-[16px]">Duration: {(new Date(item.end_date.split('T')[0])- new Date(item.start_date.split('T')[0]))/ (1000 * 60 * 60 * 24)} Days</small>
+                <div className="flex flex-col gap-1">
+                <small className="text-[#0b4626ad] drop-shadow-md font-medium text-[16px]">Duration: {(new Date(item.end_date.split('T')[0])- new Date(item.start_date.split('T')[0]))/ (1000 * 60 * 60 * 24)} Days</small>
                 {/* DESCRIPTION */}
-                <p className="text-[16px] font-medium">{item.description.slice(0,100)} <Link to={`/campaignBlog/${item.campaign_id}`} className="font-medium underline text-[#40025D]">Learn More...</Link>
+                <p className="text-[16px] font-medium">{item.description.slice(0,100)} <Link to={`/campaignBlog/${item.campaign_id}`} className="font-medium tracking-wide underline text-[#40025D]"> Learn More...</Link>
                 {/* LINK */}
                  </p>
                  
