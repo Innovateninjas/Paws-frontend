4d3ebad9330e3a48c729511529a0abb6079bc0ef
Shreya <shreyashaw1705@gmail.com>
2024-04-02 19:41:23 +0530

    Minor Fixes, Campaign Blog Skeleton

--- src/Components/utils/Functions/handleInterest.js
+++ src/Components/utils/Functions/handleInterest.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export const handleInterest = async (id, email, setInterested,setDisable) => {
   try {
+    const url = process.env.REACT_APP_BACKEND_URL;
     const response = await axios.get(
-      `https://paws-backend.azurewebsites.net/api/campaigns/${id}/`
+      `${url}/api/campaigns/${id}/`
     );
     let applicant_list = response.data.applicant_list;
     if (applicant_list === null) {
@@ -23,7 +24,7 @@ export const handleInterest = async (id, email, setInterested,setDisable) => {
       }
     );
     if (patchResponse.status === 200) {
-      setInterested("Interest Shown");
+      setInterested("Interested");
       setDisable(true);
     } else {
       console.error("Error showing interest:", patchResponse.statusText);
