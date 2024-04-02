325b5dd8197c356b07892e4dd8f509bf7029b2c2
Shreya <shreyashaw1705@gmail.com>
2024-04-02 20:28:10 +0530

    Campaign List Skeleton

--- src/Components/Skeletons/campList.jsx
+++ src/Components/Skeletons/campList.jsx
@@ -7,11 +7,11 @@ const MapSkeleton = ({ width , height, ...props }) => (
     speed={1}
     width={width}
     height={height}
-    backgroundColor="#f9f9f9"
-    foregroundColor="#e0e0e0"
+    backgroundColor="#ffffff66"
+    foregroundColor="rgba(255, 255, 255, 0.7)"
     {...props}
   >
-    <rect x="0" y="0"  rx="20" ry="20" width={width} height={height}>
+    <rect x="0" y="0"  rx="30" ry="30" width={width} height={height}>
       <animate attributeName="fill" dur="3s" repeatCount="indefinite" />
     </rect>
   </ContentLoader>
