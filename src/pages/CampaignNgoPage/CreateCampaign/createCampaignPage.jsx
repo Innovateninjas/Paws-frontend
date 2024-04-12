4d0f40cec39c9246f3e43fd9415e8313c968a6b6
Shreya <shreyashaw1705@gmail.com>
2024-04-12 17:00:58 +0530

    :) part2

--- src/pages/CampaignNgoPage/CreateCampaign/createCampaignPage.jsx
+++ src/pages/CampaignNgoPage/CreateCampaign/createCampaignPage.jsx
@@ -9,7 +9,7 @@ const CreateCampaignPage = ({ onClick }) => {
       <Background />
       <div className=" h-[60vh] flex z-[60] items-center justify-between flex-col">
 
-        <h1 className="mt-[40px] text-[#40025D] text-center w-[90vw] backdrop-blur-[6px] font-baloo text-shadow shadow-custom bg-gradient-to-b from-campaign-input-top via-campaign-input-middle to-campaign-input-bottom rounded-[50px] py-[40px] px-[20px]">
+        <h1 className="mt-[40px] text-[#40025D] text-center w-[90vw] backdrop-blur-[6px] font-baloo text-shadow shadow-custom bg-white bg-opacity-[0.5] rounded-[50px] py-[40px] px-[20px]">
 
           <p className="text-[2rem] leading-10 animate-fade-in-campaign font-extrabold flex flex-col gap-5">
             <span>Protecting Paws,</span>
