60d89be5e51b1fa35026c0ae68152aa68d8a6568
codewarnab <dp950810@gmail.com>
2024-04-13 11:44:51 +0530

    modified:   src/App.css 	modified:   src/App.jsx 	new file:   src/Components/FAQ/FAQ.jsx 	new file:   src/Components/FAQ/FAQ.module.css 	modified:   src/pages/ReportIncidentPage/IncidentMain.jsx 	modified:   src/pages/ReportIncidentPage/animalDetailspage/AnimalDetailsPage.jsx 	renamed:    src/pages/successPage/SuccessPage.jsx -> src/pages/ReportIncidentPage/successPage/SuccessPage.jsx

--- src/pages/ReportIncidentPage/successPage/SuccessPage.jsx
+++ src/pages/ReportIncidentPage/successPage/SuccessPage.jsx
@@ -1,9 +1,46 @@
 // SuccessPage.js
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
-import Background from "../../Components/backgroundComponent/Background";
+import Background from "../../../Components/backgroundComponent/Background";
+import FAQ  from "../../../Components/FAQ/FAQ";
+
 
 function SuccessPage() {
+  const toggleFAQ = index => {
+    setFaqs(
+      faqs.map((faq, i) => {
+        if (i === index) {
+          faq.open = !faq.open;
+        } else {
+          faq.open = false;
+        }
+
+        return faq;
+      })
+    );
+  };
+
+  const[faqs, setFaqs] = useState([
+    {
+      question: "How many programmers does it take to screw a lightbulb?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
+      open: true
+    },
+    {
+      question: "Who is the most awesome person?",
+      answer: "You! The viewer!",
+      open: false
+    },
+    {
+      question:
+        "How many questions does it take to makes a succesful FAQ Page?",
+      answer: "This many!",
+      open: false
+    }
+  ]);
+
+
   const containerStyle = {
     width: '155px',
     height: '50px',
@@ -27,6 +64,11 @@ function SuccessPage() {
           View Reports
         </Link>
       </div>
+        <div className="w-full max-w-[768px] mx-auto p-[13px]">
+          {faqs.map((faq, index) => (
+            <FAQ faq={faq} index={index} key={index} toggleFAQ={toggleFAQ} />
+          ))}
+        </div>
     </div>
     </div>
   );
