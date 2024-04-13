a063ba00c449bb2ab88dcb2258ac568653d04068
Arnab Mondal <dp950810@gmail.com>
2024-04-13 15:27:04 +0530

    Update SuccessPage.jsx

--- src/pages/ReportIncidentPage/successPage/SuccessPage.jsx
+++ src/pages/ReportIncidentPage/successPage/SuccessPage.jsx
@@ -22,7 +22,7 @@ function SuccessPage() {
       <div className="w-full h-60vh text-center mt-16  mb-10 pt-[7.5rem] flex flex-col items-center gap-5 justify-center">
         <h2 className="text-purple-950 text-5xl font-normal font-['Bayon'] tracking-widest">Success</h2>
         <div className="w-354 h-354 transform p-6  text-6xl origin-top-left bg-gradient-to-b from-green-300 to-purple-500 shadow-lg rounded-full shadow-black-900/60">✓</div>
-        <p p className="w-89 h-32 text-center text-indigo-950 text-opacity-90 text-2xl  font-['Bree Serif'] tracking-widest font-bold">Your report has been successfully submitted</p>
+        <p  className="w-89 h-32 text-center text-indigo-950 text-opacity-90 text-2xl  font-['Bree Serif'] tracking-widest font-bold">Your report has been successfully submitted</p>
         <div className="flex flex-row items-center w-full justify-evenly">
           <Link to="/" className="w-472 h-175 bg-opacity-40 bg-white shadow-lg rounded-full border-3 p-3 backdrop-blur-2xl text-black text-xl font-normal font-bakbak-one  shadow-black-900/60 hover:shadow-indigo-500/40 ">
             Back to Home
