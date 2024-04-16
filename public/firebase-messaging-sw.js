504cfee7fc6216f1606d4a2e58afc3d0cd15459c
rishicds <124495375+rishicds@users.noreply.github.com>
2024-04-16 10:02:30 +0530

    Final final

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -21,7 +21,7 @@ messaging.onBackgroundMessage((payload) => {
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: payload.notification.icon
+        icon: './notification_icon.ico'
     };
 
     self.registration.showNotification(notificationTitle,
