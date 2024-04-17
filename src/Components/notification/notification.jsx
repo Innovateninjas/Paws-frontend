08cac008888b7b26700a617ede3fabbe2c670c69
Anirban Majumder <81350357+anirbanmajumder0@users.noreply.github.com>
2024-04-17 16:44:49 +0530

    Refactor notification component to include a link to the dashboard and display the number of filtered reports

--- src/Components/notification/notification.jsx
+++ src/Components/notification/notification.jsx
@@ -1,18 +1,23 @@
-import React, { useState } from 'react'
+import React from 'react';
 import { IoNotifications } from "react-icons/io5";
+import { Link } from 'react-router-dom';
 const Notification = ({ onClick, reports }) => {
-    const [showDot, setShowDot] = useState(false);    
     const filteredReports = reports.filter(report => report.status === "In Progress" || report.status === "Received");
     console.log(filteredReports);
-  return (
-    <>
-     <div className="flex w-screen justify-end relative" onClick={() => { onClick(); setShowDot((prev)=> !prev); }}>
-        <IoNotifications style={{ margin: '20px'}} fontSize='30px' color='rgb(255,255,255)' />
-        {showDot && <div style={{ position: 'absolute', top: '15px', right: '20px', width: '10px', height: '10px', borderRadius: '50%', backgroundColor: 'red' }} />}
-    </div>
-    </>
-   
-  )
+
+    return (
+        <>
+            <Link to="/dashboard">
+            <div className="flex w-screen justify-end relative">
+                <IoNotifications style={{ margin: '20px'}} fontSize='30px' color='rgb(255,255,255)' />
+                
+                <div className="px-2 py-[3px]" style={{ position: 'absolute', top: '5px', right: '10px', backgroundColor: 'red',fontSize:'14px',borderRadius: '50%', color: 'white' }}>
+                    {filteredReports.length}
+                </div>
+            </div>
+            </Link>
+        </>
+    )
 }
 
 export default Notification
