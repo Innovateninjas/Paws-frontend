8fd4875689b049e24711984ca75ca2b21d463d35
rishicds <124495375+rishicds@users.noreply.github.com>
2024-04-29 01:19:32 +0530

    added member

--- src/pages/shared/team.jsx
+++ src/pages/shared/team.jsx
@@ -6,11 +6,21 @@ const TeamPage = () => {
     {
       name: "Rishi Paul",
       position: "AI Engineer",
-      image: "https://media.licdn.com/dms/image/D5603AQH5Pdev9HMjdw/profile-displayphoto-shrink_800_800/0/1702138868459?e=1717027200&v=beta&t=67GlMCnOOMNe-dqJ5YfbeY3dgaGjQA1j935ImKW3Dyc",
+      image: "https://media.licdn.com/dms/image/D5603AQFcc8_vrp3w-Q/profile-displayphoto-shrink_800_800/0/1714159671557?e=1720051200&v=beta&t=fWJEx0tXqTQdx1xN63fDZW50kS_LAZStD9TyF67le_k",
       featureText: "Tests in Production, because who needs that silly staging environment anyway?",
       socialLinks: {
         linkedin: "https://www.linkedin.com/in/rishi-paul04/",
         github: "https://github.com/rishicds"
+      }
+    },
+    {
+      name: "Aditi Ghosh",
+      position: "UI Developer",
+      image: "https://res.cloudinary.com/dff97ky68/image/upload/v1714333558/WhatsApp_Image_2024-04-29_at_12.31.12_AM_ohouce.jpg",
+      featureText: "Code Fearlessly, Innovate Relentlessly.",
+      socialLinks: {
+        linkedin: "https://www.linkedin.com/in/aditighosh2005",
+        github: "https://github.com/GhoshAditi"
       }
     },
     {
@@ -24,16 +34,6 @@ const TeamPage = () => {
       }
     },
     {
-      name: "Arnab Mondal",
-      position: "Frontend Developer",
-      image: "https://media.licdn.com/dms/image/D5603AQF1SG1K-VPsTg/profile-displayphoto-shrink_800_800/0/1699975516118?e=1717027200&v=beta&t=ZLzBL-4382gD16CTtZhR9aEEz6El5CBF8L_ImUUVz3w",
-      featureText: "it's not a bug; it's an undocumented feature",
-      socialLinks: {
-        linkedin: "https://in.linkedin.com/in/arnab-mondal-117616278",
-        github: "https://github.com/codewarnab"
-      }
-    },
-    {
       name: "Shreya Shaw",
       position: "Frontend Developer & Ui/Ux Designer",
       image: "https://media.licdn.com/dms/image/D5603AQFAH-ciYtM0ZA/profile-displayphoto-shrink_800_800/0/1711645818457?e=1717027200&v=beta&t=lQ0H6HJRfpEhlorEc4OhMNNC_sPgSQTDtPHgq47M9K4",
@@ -44,24 +44,36 @@ const TeamPage = () => {
       }
     },
     {
-      name: "Debayudh Basu",
+      name: "Arnab Mondal",
       position: "Frontend Developer",
-      image: "https://media.licdn.com/dms/image/D5603AQE3L9nP5ZM_vg/profile-displayphoto-shrink_800_800/0/1708620931019?e=1717027200&v=beta&t=mJwwcnkLJNBYUAa4vzFqXGe0C4TVOAwD5iD9eJih0lY",
-      featureText: "Uncover the Invisible: Explore Network Traffic with Precision",
+      image: "https://media.licdn.com/dms/image/D5603AQF1SG1K-VPsTg/profile-displayphoto-shrink_800_800/0/1699975516118?e=1717027200&v=beta&t=ZLzBL-4382gD16CTtZhR9aEEz6El5CBF8L_ImUUVz3w",
+      featureText: "it's not a bug; it's an undocumented feature",
       socialLinks: {
-        linkedin: "https://www.linkedin.com/in/debayudh-basu-5280562b2/",
-        github: "https://github.com/debayudh07"
+        linkedin: "https://in.linkedin.com/in/arnab-mondal-117616278",
+        github: "https://github.com/codewarnab"
       }
     },
     {
       name: "Sutanuka Chakraborty",
-      position: "ui/ux designer ",
+      position: "UI/UX Designer",
       image: "https://media.licdn.com/dms/image/D4D03AQHYnR-kriTWSQ/profile-displayphoto-shrink_200_200/0/1713470417629?e=1718841600&v=beta&t=CayRuA_fpxqoUydhR-bMDvhAa2bnah_9Lvm_KbsV61k",
       featureText: 'Make sure to always "make it pop"!',
       socialLinks: {
         linkedin: "https://www.linkedin.com/in/sutanuka-chakraborty-148744275/",
       }
     },
+    
+    {
+      name: "Debayudh Basu",
+      position: "Frontend Developer",
+      image: "https://media.licdn.com/dms/image/D5603AQE3L9nP5ZM_vg/profile-displayphoto-shrink_800_800/0/1708620931019?e=1717027200&v=beta&t=mJwwcnkLJNBYUAa4vzFqXGe0C4TVOAwD5iD9eJih0lY",
+      featureText: "Who needs cocaine when you can sniff HTTP requests?",
+      socialLinks: {
+        linkedin: "https://www.linkedin.com/in/debayudh-basu-5280562b2/",
+        github: "https://github.com/debayudh07"
+      }
+    },
+    
     {
       name: "Chandrima Ghosh",
       position: "QA & testing Engineer",
@@ -78,10 +90,10 @@ const TeamPage = () => {
   ];
 
   return (
-    <div className="bg-gradient-to-r from-blue-500 to-green-500 min-h-screen pd-4">
+    <div className="bg-gradient-to-r from-blue-500 to-green-500 min-h-screen pd-4 pb-4">
       <div className="container mx-auto px-4 py-8">
         <h2 className="text-3xl font-semibold text-center mb-8 text-white">Our Team</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 justify-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-center pb-4">
           {teamMembers.map((member, index) => (
             <div key={index} className="w-[23rem] z-[30] bg-white rounded-3xl bg-opacity-57 backdrop-blur-[5px] shadow-lg ring-1 ring-gray-300">
               <img className="w-60 mt-2 h-60 rounded-full border-4 border-emerald-500 mx-auto block" src={member.image} alt={member.name} />
