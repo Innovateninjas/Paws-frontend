9654d72d51a8ac19b6af3ee6fac2422b1b632fbc
dev-rsiva <dev.rsiva@gmail.com>
2024-05-11 17:37:18 +0530

    Updated the UI for edit/save functionality

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,15 +2,25 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBgRI7QrEqlrtj94hZ-MKtYc77XunOc_9w",
+//     authDomain: "paws-b9b64.firebaseapp.com",
+//     projectId: "paws-b9b64",
+//     storageBucket: "paws-b9b64.appspot.com",
+//     messagingSenderId: "610215952536",
+//     appId: "1:610215952536:web:4f832813ef5d58acb010ed",
+//     measurementId: "G-3TBQ0B47KK"
+// };
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBgRI7QrEqlrtj94hZ-MKtYc77XunOc_9w",
-    authDomain: "paws-b9b64.firebaseapp.com",
-    projectId: "paws-b9b64",
-    storageBucket: "paws-b9b64.appspot.com",
-    messagingSenderId: "610215952536",
-    appId: "1:610215952536:web:4f832813ef5d58acb010ed",
-    measurementId: "G-3TBQ0B47KK"
-};
+    apiKey: "AIzaSyB3IgAfYnYRD2f3_SiV8YTdb5xD0QEC2Ps",
+    authDomain: "paws-1eb95.firebaseapp.com",
+    projectId: "paws-1eb95",
+    storageBucket: "paws-1eb95.appspot.com",
+    messagingSenderId: "120768381104",
+    appId: "1:120768381104:web:c2695466eabd7813b5fb75"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
--- src/pages/user/UserProfile.jsx
+++ src/pages/user/UserProfile.jsx
@@ -1,21 +1,51 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import { FaUser, FaPhoneAlt } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import { RiCoinsLine } from "react-icons/ri";
 import { IoDocumentSharp } from "react-icons/io5";
+import { FaPencil } from "react-icons/fa6";
+import { MdErrorOutline } from "react-icons/md";
+import isValidPhoneNumber from "../../utils/Functions/phoneNumberValidator";
 import Loader from "../../Components/shared/loader";
 import { Link } from "react-router-dom";
 import { UserContext } from "../../utils/contexts/UserContext";
 import { HiMiniUser } from "react-icons/hi2";
 function UserPage() {
+  // const mockData = {
+  //   name: "siva",
+  //   email: "siva.torres@gmail.com",
+  //   phone_number: 8484848499,
+  //   no_reports: 5,
+  //   coins: 4,
+  // };
   const [userDetails, setUserData] = useState(null);
   const { userData, loading, error } = useContext(UserContext);
+  const [edit, setEdit] = useState(null);
+  const [errorMsgForName, setErrorMsgForName] = useState(null);
+  const [errorMsgForPhoneNumber, setaErrorMsgForPhoneNumber] = useState(null);
+
+  const nameRef = useRef(null);
+  const phoneNumberRef = useRef(null);
+
+  console.log(nameRef);
+  console.log(userDetails?.phone_number);
 
   useEffect(() => {
     if (!loading && !error && userData) {
       setUserData(userData);
     }
   }, [userData, loading, error]);
+
+  const updateUserData = (name, value) => {
+    setUserData((prev) => {
+      let updatedUserData = {
+        ...prev,
+        [name]: [value],
+      };
+      console.log(updatedUserData);
+      return updatedUserData;
+    });
+  };
 
   if (error) {
     return <h1>{error}</h1>;
@@ -30,47 +60,157 @@ function UserPage() {
       <div className="flex flex-col font-Calistoga items-center justify-center gap-[30px] h-screen bg-custom-gradient">
         <div className="p-5 rounded-[50%] bg-blue-900 shadow- dashBoardCardImageShadow flex items-end justify-center ">
           {/* <img className="" src="./images/profileIcon.png" alt="icon" /> */}
-              <HiMiniUser fontSize="9rem" color="rgba(255,255,255,0.8)" />
+          <HiMiniUser fontSize="9rem" color="rgba(255,255,255,0.8)" />
         </div>
         <div className=" min-w-80 p-5 border-1 flex flex-col gap-[15px] rounded-[30px]  ring-1 ring-gray-300 bg-opacity-35 bg-white shadow-dashBoardCardImageShadow backdrop-blur-[5px]">
           {/* NAME */}
-          <div className="flex gap-1 items-center text-xl drop-shadow-md">
+          <div className="flex gap-1 items-center text-xl drop-shadow-md relative">
             <span className="rounded-full bg-blue-900 p-2 mr-2">
               <FaUser color="rgba(255,255,255,0.7)" />
             </span>
-            <span>{userDetails.name}</span>
+            {edit !== "name" && <span>{userDetails?.name}</span>}
+            {edit === "name" && (
+              <>
+                <div className="flex justify-center items-center absolute left-12 gap-1">
+                  <input
+                    ref={nameRef}
+                    defaultValue={userDetails?.name}
+                    type="text"
+                    className="max-w-[170px] p-2 rounded outline-none"
+                  />
+                  <button
+                    className="bg-blue-600 rounded text-white p-2"
+                    onClick={() => {
+                      if (nameRef?.current?.value?.trim().length >= 3) {
+                        updateUserData("name", nameRef?.current?.value);
+                        setEdit(null);
+                        setErrorMsgForName(null);
+                      } else {
+                        setErrorMsgForName(
+                          "Name should be atleast 3 characters"
+                        );
+                      }
+                    }}
+                  >
+                    Save
+                  </button>
+                  <button
+                    className="bg-gray-200 rounded text-black p-2"
+                    onClick={() => setEdit(null)}
+                  >
+                    Cancel
+                  </button>
+                  {errorMsgForName !== null && (
+                    <div className="text-red-600 text-base ml-6 min-w-[280px] flex items-center">
+                      <span>
+                        <MdErrorOutline className="text-2xl mr-2" />
+                      </span>
+                      {errorMsgForName}
+                    </div>
+                  )}
+                </div>
+              </>
+            )}
+
+            <div className="w-full flex justify-end text-right cursor-pointer text-base text-gray-900 hover:text-blue-900">
+              <div
+                onClick={() => {
+                  setEdit("name");
+                  setTimeout(() => {
+                    nameRef?.current?.focus();
+                  }, 0);
+                }}
+              >
+                <FaPencil />
+              </div>
+            </div>
           </div>
           {/* Email */}
           <div className="flex gap-1 items-center text-xl drop-shadow-md">
             <span className="rounded-full bg-blue-900 p-1 mr-2">
               <IoIosMail fontSize="30px" color="rgba(255,255,255,0.7)" />
             </span>
-            {userDetails.email}
+            {userDetails?.email}
           </div>
           {/* PHONE NUMBER */}
-          <div className="flex items-center gap-1 text-xl drop-shadow-md">
+          <div className="flex items-center gap-1 text-xl drop-shadow-md relative">
             <span className="rounded-full bg-blue-900 p-2 mr-2">
               <FaPhoneAlt fontSize="22px" color="rgba(255,255,255,0.7)" />
             </span>
-            <span>{userDetails.phone_number}</span>
+            {edit !== "phoneNumber" && <span>{userDetails?.phone_number}</span>}
+            {edit === "phoneNumber" && (
+              <>
+                <div className="flex justify-center items-center absolute left-12 gap-1">
+                  <input
+                    ref={phoneNumberRef}
+                    defaultValue={userDetails?.phone_number}
+                    type="text"
+                    className="max-w-[170px] p-2 rounded outline-none"
+                  />
+                  <button
+                    className="bg-blue-600 rounded text-white p-2"
+                    onClick={() => {
+                      if (isValidPhoneNumber(phoneNumberRef?.current?.value)) {
+                        updateUserData(
+                          "phone_number",
+                          phoneNumberRef?.current.value
+                        );
+                        setEdit(null);
+                        setaErrorMsgForPhoneNumber(null);
+                      } else {
+                        setaErrorMsgForPhoneNumber("Phone number is invalid");
+                      }
+                    }}
+                  >
+                    Save
+                  </button>
+                  <button
+                    className="bg-gray-200 rounded text-black p-2"
+                    onClick={() => setEdit(null)}
+                  >
+                    Cancel
+                  </button>
+                  {errorMsgForPhoneNumber !== null && (
+                    <div className="text-red-600 text-base ml-6 min-w-[280px] flex items-center">
+                      <span>
+                        <MdErrorOutline className="text-2xl mr-2" />
+                      </span>
+                      {errorMsgForPhoneNumber}
+                    </div>
+                  )}
+                </div>
+              </>
+            )}
+            <div className="w-full flex justify-end text-right cursor-pointer text-base text-gray-900 hover:text-blue-900">
+              <div>
+                <FaPencil
+                  onClick={() => {
+                    setEdit("phoneNumber");
+                    setTimeout(() => {
+                      phoneNumberRef?.current?.focus();
+                    }, 0);
+                  }}
+                />
+              </div>
+            </div>
           </div>
 
           {/* NUMBER OF REPORTS */}
           <div className="flex items-center gap-1 text-xl drop-shadow-md">
-          <span className="rounded-full bg-blue-900 p-2 mr-2">
-          <IoDocumentSharp fontSize="24px" color="rgba(255,255,255,0.7)"/>
+            <span className="rounded-full bg-blue-900 p-2 mr-2">
+              <IoDocumentSharp fontSize="24px" color="rgba(255,255,255,0.7)" />
               {/* <FaPhoneAlt fontSize="22px" color="rgba(255,255,255,0.7)" /> */}
             </span>
-          
-            <span>Reports: {userDetails.no_reports}</span>
+
+            <span>Reports: {userDetails?.no_reports}</span>
           </div>
 
           {/* COINS */}
           <p className="flex items-center gap-1 drop-shadow-sm text-xl">
-          <span className="rounded-full bg-blue-900 p-2 mr-2">
-          <RiCoinsLine fontSize="28px" color="rgba(255,255,255,0.7)" />
+            <span className="rounded-full bg-blue-900 p-2 mr-2">
+              <RiCoinsLine fontSize="28px" color="rgba(255,255,255,0.7)" />
             </span>
-            <span className="tracking-wider">Coins: {userDetails.coins}</span>
+            <span className="tracking-wider">Coins: {userDetails?.coins}</span>
           </p>
         </div>
         {/* BUTTONS */}
