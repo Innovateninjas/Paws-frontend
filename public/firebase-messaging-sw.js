7c8041a5a2cfc67f4b0730d797e52db7a591acbc
ADARSH AMIT <adarsh.amit1001@gmail.com>
2024-05-12 02:28:45 +0530

    Update firebase-messaging-sw.js
    removed unnecessary commented part.

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,16 +4,6 @@ importScripts(
   "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
 );
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBgRI7QrEqlrtj94hZ-MKtYc77XunOc_9w",
-//     authDomain: "paws-b9b64.firebaseapp.com",
-//     projectId: "paws-b9b64",
-//     storageBucket: "paws-b9b64.appspot.com",
-//     messagingSenderId: "610215952536",
-//     appId: "1:610215952536:web:4f832813ef5d58acb010ed",
-//     measurementId: "G-3TBQ0B47KK"
-// };
-
 const firebaseConfig = {
   apiKey: "AIzaSyBgRI7QrEqlrtj94hZ-MKtYc77XunOc_9w",
   authDomain: "paws-b9b64.firebaseapp.com",
