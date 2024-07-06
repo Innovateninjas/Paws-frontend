7e71a40a9fbdcd7de43334eb51b4253c459ca007
Anagha Chaudhari <143149376+anagha-chaudhari@users.noreply.github.com>
2024-07-06 15:11:09 +0530

    added a happy recovery story page

--- src/Components/shared/Navbar.jsx
+++ src/Components/shared/Navbar.jsx
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import { FaUser, FaUserPlus} from 'react-icons/fa';
+import { FaUser, FaUserPlus, FaPaw } from 'react-icons/fa';
 import { PiHandCoinsBold } from "react-icons/pi";
 import { MdCampaign, MdHome, MdContentPasteSearch } from "react-icons/md";
 import { IoPeople } from "react-icons/io5";
 import { ImStatsBars } from "react-icons/im";
-
-
 
 const Navbar = () => {
   const csrftoken = localStorage.getItem('csrftoken');
@@ -25,55 +23,57 @@ const Navbar = () => {
 
   return (
     <div className='fixed bottom-0 left-0 w-full z-50'>
-    <nav className='nav'>
-      <BottomNavigation
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        style={{
-          width: '100%',
-          margin: '0',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-evenly',
-          borderTopLeftRadius: '16px',
-          borderTopRightRadius: '16px',
-          backgroundColor: '#b8ddfa',
-        }}
-      >
-        {csrftoken && userType === "user" ? (
-          [
-            <BottomNavigationAction 
-            key="home" value="/" component={Link} 
-            to="/" icon={<MdHome size={30}/>} 
-            />,
-            <BottomNavigationAction key="campaignList" value="/campaignList" component={Link} to="/campaignList" icon={<IoPeople size={28} />} />,
-            <BottomNavigationAction key="donationfront" value="/donationfront" component={Link} to="/donationfront" icon={<PiHandCoinsBold size={28} />} />,
-            <BottomNavigationAction key="profile" value="/user" component={Link} to="/user" icon={<FaUser size={24} />} />,
-          ]
-        ) : csrftoken && userType === "ngo" ? (
-          [
-            <BottomNavigationAction key="stats" value="/stats" component={Link} to="/stats" icon={<ImStatsBars size={26}/>}/>,
-            <BottomNavigationAction key="reports" value="/dashboard" component={Link} to="/dashboard" icon={<MdContentPasteSearch size={28}/>} />,
-            <BottomNavigationAction key="createcampaign" value="/createcampaign" component={Link} to="/createcampaign" icon={<MdCampaign size={32} style={{ transform: 'rotate(-15deg)' }} />} />,
-            <BottomNavigationAction key="ngoprofile" value="/ngoProfile" component={Link} to="/ngoProfile" icon={<FaUser size={24} />} />,
-          ]
-        )
-          : (
+      <nav className='nav'>
+        <BottomNavigation
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          style={{
+            width: '100%',
+            margin: '0',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-evenly',
+            borderTopLeftRadius: '16px',
+            borderTopRightRadius: '16px',
+            backgroundColor: '#b8ddfa',
+          }}
+        >
+          {csrftoken && userType === "user" ? (
             [
               <BottomNavigationAction 
-              key="home" value="/" component={Link} 
-              to="/" icon={<MdHome size={30}/>} 
-              showLabel
-              label = "Home"  
+                key="home" value="/" component={Link} 
+                to="/" icon={<MdHome size={30}/>} 
+              />,
+              <BottomNavigationAction key="campaignList" value="/campaignList" component={Link} to="/campaignList" icon={<IoPeople size={28} />} />,
+              <BottomNavigationAction key="donationfront" value="/donationfront" component={Link} to="/donationfront" icon={<PiHandCoinsBold size={28} />} />,
+              <BottomNavigationAction key="Testimonials" value="/Testimonials" component={Link} to="/Testimonials" icon={<FaPaw size={28} />} />, // for recovered stories icon
+              <BottomNavigationAction key="profile" value="/user" component={Link} to="/user" icon={<FaUser size={24} />} />,
+            ]
+          ) : csrftoken && userType === "ngo" ? (
+            [
+              <BottomNavigationAction key="stats" value="/stats" component={Link} to="/stats" icon={<ImStatsBars size={26}/>}/>,
+              <BottomNavigationAction key="reports" value="/dashboard" component={Link} to="/dashboard" icon={<MdContentPasteSearch size={28}/>} />,
+              <BottomNavigationAction key="createcampaign" value="/createcampaign" component={Link} to="/createcampaign" icon={<MdCampaign size={32} style={{ transform: 'rotate(-15deg)' }} />} />,
+              <BottomNavigationAction key="Testimonials" value="/Testimonials" component={Link} to="/Testimonials" icon={<FaPaw size={28} />} />, // for recovered stories icon
+              <BottomNavigationAction key="ngoprofile" value="/ngoProfile" component={Link} to="/ngoProfile" icon={<FaUser size={24} />} />,
+            ]
+          ) : (
+            [
+              <BottomNavigationAction 
+                key="home" value="/" component={Link} 
+                to="/" icon={<MdHome size={30}/>} 
+                showLabel
+                label="Home"  
               />,
               <BottomNavigationAction key="register" value="/register" showLabel label="Register" component={Link} to="/register" icon={<FaUserPlus size={28} />} />,
               <BottomNavigationAction key="login" value="/login" showLabel label="Login" component={Link} to="/login" icon={<FaUser size={24} />} />,
+              <BottomNavigationAction key="RecoveredsPage" value="/recovereds" showLabel label="Stories" component={Link} to="/recovereds" icon={<FaPaw size={28} />} />, // for recovered stories section
             ]
           )}
-      </BottomNavigation>
-    </nav>
+        </BottomNavigation>
+      </nav>
     </div>
   );
 };
